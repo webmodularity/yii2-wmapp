8ccff774301ae0e954decb542b911c539fd893fb
Rory Hellyer <rory@webmodularity.com>
2016-05-12 00:40:35 +0000

    wm modifications

--- environments/prod/backend/web/index.php
+++ environments/prod/backend/web/index.php
@@ -14,5 +14,5 @@ $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/main-local.php')
 );
 
-$application = new yii\web\Application($config);
+$application = new wma\web\Application($config);
 $application->run();
