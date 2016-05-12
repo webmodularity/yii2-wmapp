f94ba7c59f61992b86a061fc2f919960b3d23a5c
Rory Hellyer <rory@webmodularity.com>
2016-05-12 08:19:11 +0000

    Remove backend SiteController and replace with DashboardController

---
+++ backend/controllers/DashboardController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace backend\controllers;
+
+class DashboardController extends \wma\controllers\DashboardController
+{
+
+
+}
---
+++ backend/views/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = 'Dashboard';
+$this->params['wma-nav'] = "Dashboard";
+$this->params['breadcrumbs'][] = 'Dashboard';
+?>
+<div class="row">
+
+</div>
